7333bc1d824cbb06bcf9095bda5013d438c02057
belyakov <belyakov@intervolga.ru>
2024-03-11 23:20:40 +0300

    Реализовал переход по URL

--- Scripts/Tools/Tools.cs
+++ Scripts/Tools/Tools.cs
@@ -39,6 +39,15 @@ namespace Lancy.Tools
         }
 
         /// <summary>
+        /// Метод, который позволяет открыть какую-либо ссылку
+        /// </summary>
+        /// <param name="URl">Путь к странице</param>
+        public void OpenURL(string URl)
+        {
+            Application.OpenURL(URl);
+        }
+
+        /// <summary>
         /// Метод, который позволяет выйти из игры
         /// </summary>
         public void Quit()
