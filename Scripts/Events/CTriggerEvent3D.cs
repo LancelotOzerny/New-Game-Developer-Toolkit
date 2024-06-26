79dfefa7323dff9a72296d2bc8670d572cd4441c
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-06-27 02:49:23 +0300

    Доабвил режим отладки в триггер 3D объектов

--- Scripts/Events/CTriggerEvent3D.cs
+++ Scripts/Events/CTriggerEvent3D.cs
@@ -4,8 +4,15 @@ namespace Lancy.Events
 {
     public class CTriggerEvent3D : CSingletoonTagEvent
     {
+        [SerializeField] private bool debugMode;
+
         private void OnTriggerEnter(Collider collision)
         {
+            if (debugMode)
+            {
+                Debug.Log($"{this.name}: Trigger with object \"{collision.tag}\"");
+            }
+
             InvokeEventsByGameObject(collision.gameObject);
         }
     }
